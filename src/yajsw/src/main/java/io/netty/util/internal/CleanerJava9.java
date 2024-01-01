a477f4f58c5ca640f1f2874ee953bc1e95bff0ad
rzo <rzo@gmx.de>
2024-01-01 10:27:59 +0100

    release 13.11 missing changes

--- src/yajsw/src/main/java/io/netty/util/internal/CleanerJava9.java
+++ src/yajsw/src/main/java/io/netty/util/internal/CleanerJava9.java
@@ -5,7 +5,7 @@
 * version 2.0 (the "License"); you may not use this file except in compliance
 * with the License. You may obtain a copy of the License at:
 *
-*   http://www.apache.org/licenses/LICENSE-2.0
+*   https://www.apache.org/licenses/LICENSE-2.0
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -15,14 +15,14 @@
 */
 package io.netty.util.internal;
 
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-
-import io.netty.util.internal.logging.InternalLogger;
-import io.netty.util.internal.logging.InternalLoggerFactory;
 
 /**
  * Provide a way to clean a ByteBuffer on Java9+.
@@ -70,8 +70,7 @@ final class CleanerJava9 implements Cleaner {
         if (error == null) {
             logger.debug("java.nio.ByteBuffer.cleaner(): available");
         } else {
-            //logger.debug("java.nio.ByteBuffer.cleaner(): unavailable", error);
-        	logger.debug("java.nio.ByteBuffer.cleaner(): unavailable");
+            logger.debug("java.nio.ByteBuffer.cleaner(): unavailable", error);
         }
         INVOKE_CLEANER = method;
     }
